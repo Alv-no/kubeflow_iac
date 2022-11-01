7b6a620a58b711ba1fffafee6aa32d326993bc85
arturo_opsetmoen <arturo.opsetmoen@amador.no>
2022-11-01 13:45:35 +0100

    build(scripts): Minor changes

--- deploy_kubeflow.sh
+++ deploy_kubeflow.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
+# Deploy the (full-)kubeflow bundle. 
+# For systems with limited resources, 
+# use "kubeflow-lite" instead of "kubeflow".
 juju deploy kubeflow --trust
 
+# Local configurations
+# Configure auth and access to kubeflow dashboard
+juju config dex-auth public-url=http://10.64.140.43.nip.io
+juju config oidc-gatekeeper public-url=http://10.64.140.43.nip.io
+# Set up the user name and passwords
 juju config dex-auth static-username=admin
 juju config dex-auth static-password=admin
 
--- install_juju.sh
+++ install_juju.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
+# Install the juju olm from snap.
 sudo snap install juju --classic
-
+# Deploy the juju controller to the kubernetes 
+# cluster set up with microk8s.
 juju bootstrap microk8s
+# Set up a specific model for kubeflow.
+# Model name must be kubeflow.
 juju add-model kubeflow
 
